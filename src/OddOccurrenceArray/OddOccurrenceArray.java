aa36c201eec98e12eadf035a8e57fd873b9b05f8
ManujayaPremathilaka <manujayapremathilaka@gmail.com>
2024-06-30 20:55:43 +0530

    Odd occurrence with XOR operation

--- src/OddOccurrenceArray/OddOccurrenceArray.java
+++ src/OddOccurrenceArray/OddOccurrenceArray.java
@@ -5,19 +5,8 @@ public class OddOccurrenceArray {
     public int getOddOccurrenceNo(int[] arr){
         int oddOccurrence = 0;
 
-        for (int i = 0; i < arr.length; i++){
-            oddOccurrence = arr[i];
-            for (int j = 0; j < arr.length; j++){
-                if (j == i){
-                    continue;
-                } else if (arr[j] == oddOccurrence) {
-                    oddOccurrence = 0;
-                    break;
-                }
-            }
-            if (oddOccurrence != 0){
-                break;
-            }
+        for (int k : arr) {
+            oddOccurrence ^= k;
         }
 
         return oddOccurrence;
